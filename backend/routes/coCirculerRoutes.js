1043bc562d14b27adf7610301082c583a452e102
fThAbhishek-Pandey <abhishek830564@email.com>
2025-02-13 19:16:43 +0530

    jwt added for cocirculer

--- backend/routes/coCirculerRoutes.js
+++ backend/routes/coCirculerRoutes.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import authCociculer from '../middleware/authentication/cocirculerAuth.js';
+import loginCociculer from '../middleware/authentication/logincocerculer.js';
 const coCirculerRoutes = express.Router();
-
+coCirculerRoutes.post('/login', loginCociculer);
+coCirculerRoutes.get('/auth', authCociculer);
 export default coCirculerRoutes
