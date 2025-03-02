2266dc6233f5226c9db5f3f2436a51146d7ab195
fThAbhishek-Pandey <abhishek830564@email.com>
2025-03-02 21:49:40 +0530

    adding testimorals

--- koshish/src/context/App.jsx
+++ koshish/src/context/App.jsx
@@ -3,11 +3,13 @@ import { createContext, useState } from "react";
 import getHeader from "../utils/getHeader";
 import getTopmentor from '../utils/getTopmentor'
 import getHomeEvent from "../utils/getEvent";
+import getTestimorals from "../utils/getTestimorals";
 export const AppContext = createContext(1);
 const  AppContextProvider = (props) => {
  const [headerData,setHeaderData ] = useState([]);
  const [TopMentor,setTopMentor ] = useState([]);
  const [homeEvent, setHomeEvent] = useState([])
+ const [testimorals, setTestimorals] = useState([]);
  const backendURL = import.meta.env.VITE_BACKEND_URL
 const handleHeader = ()=>{
   getHeader(backendURL, setHeaderData)
@@ -18,9 +20,14 @@ const handleTopMentor = ()=>{
 const handleHomeEvent = ()=>{
   getHomeEvent(backendURL,setHomeEvent)
 }
+const handelTestimorals =()=>{
+  getTestimorals(backendURL,setTestimorals)
+}
 const value = {
-      headerData,setHeaderData ,handleHeader,TopMentor,setTopMentor,handleTopMentor,
-      homeEvent, setHomeEvent,handleHomeEvent
+      headerData,setHeaderData ,handleHeader,
+      TopMentor,setTopMentor,handleTopMentor,
+      homeEvent, setHomeEvent,handleHomeEvent,
+      testimorals, setTestimorals,handelTestimorals
  }
 return (
     <>
